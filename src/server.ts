4c1961ab0a3698e9f68fbcf0238970b0e54466da
Jacinth <jacinthcedricbarral@gmail.com>
2026-04-21 03:35:15 +0800

    style: clean up redundant whitespace in src/server.ts to maintain consistent code formatting across the project

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-
 app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
 
 app.use('/accounts', accountsController);
