c3da93d015a3c8ead0fab9dbad16904bf1e62758
Jacinth <jacinthcedricbarral@gmail.com>
2026-04-21 01:28:10 +0800

    feat: register accounts controller middleware on the /accounts route and update the server listening log for better clarity

--- src/server.ts
+++ src/server.ts
@@ -4,19 +4,20 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import errorHandler from './_middleware/_error-handler';
 import swaggerDocs from './_helpers/swagger'
+import accountsController from './accounts/account.controller'
 
 const app = express();
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
 
-app.use(cors({origin: (origin,callback) => callback(null, true), credentials: true}));
+app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
 
-// app.use('/accounts', accountsController);
+app.use('/accounts', accountsController);
 app.use('/api-docs', swaggerDocs);
 
 app.use(errorHandler);
 
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80 ):4000;
-app.listen(port, () => console.log('Server listening on port' + port))
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+app.listen(port, () => console.log('Server listening on port: ' + port))
